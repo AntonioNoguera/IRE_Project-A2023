0a020c44ea2e35bf77db51c480cec3c3bf69599f
AntonioNoguera <michael.noguerag@uanl.edu.mx>
2023-10-14 02:31:43 -0600

    COR Mapping Added

--- IRE_Project/src/main/java/com/PI_AGO23/IRE_Project/IreProjectApplication.java
+++ IRE_Project/src/main/java/com/PI_AGO23/IRE_Project/IreProjectApplication.java
@@ -2,6 +2,9 @@ package com.PI_AGO23.IRE_Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IreProjectApplication {
@@ -10,5 +13,14 @@ public class IreProjectApplication {
 		SpringApplication.run(IreProjectApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
 
