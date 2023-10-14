6712d2330cd94823b592ca696b78cafa4169c260
AntonioNoguera <michael.noguerag@uanl.edu.mx>
2023-10-14 03:22:50 -0600

    Cors Open - Must Be Secured

--- IRE_Project/src/main/java/com/PI_AGO23/IRE_Project/IreProjectApplication.java
+++ IRE_Project/src/main/java/com/PI_AGO23/IRE_Project/IreProjectApplication.java
@@ -13,5 +13,17 @@ public class IreProjectApplication {
 		SpringApplication.run(IreProjectApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+
+	public static class CorsConfig {
+	}
 }
 
